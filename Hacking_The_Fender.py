162baf2836e8f51de949c5a7850082cc66336a2b
Anamaria Huluba <82522372+anamaria-huluba@users.noreply.github.com>
2021-11-25 19:36:48 +0000

    Update Hacking_The_Fender.py

--- Hacking_The_Fender.py
+++ Hacking_The_Fender.py
@@ -21,7 +21,10 @@ with open('passwords.csv') as password_file:
   password_csv = csv.DictReader(password_file)
   for password_row in password_csv:
     print(password_row['Username']) 
+    
 
 
 
+    
+ 
 
